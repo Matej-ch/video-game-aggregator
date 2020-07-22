e6a9af4bba853e050d68a0fd941b0f8a7cee9491
Matej <matejchalachan@gmail.com>
2020-07-22 20:11:05 +0200

    empty tiles when loading data

--- resources/views/livewire/coming-soon-games.blade.php
+++ resources/views/livewire/coming-soon-games.blade.php
@@ -17,6 +17,17 @@
             </div>
         @endif
     @empty
-        <div class="spinner mt-8"></div>
+        @foreach(range(1,3) as $game)
+            <div class="most-anticipated-container space-y-10 mt-8">
+                <div class="game flex">
+                    <div class="bg-gray-800 w-16 h-20 flex-none"></div>
+
+                    <div class="ml-4">
+                        <div class="rounded leading-tight bg-gray-700 text-transparent">game title is here</div>
+                        <div class="rounded leading-tight text-sm mt-2 bg-gray-700 inline-block text-transparent">date is here</div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     @endforelse
 </div>
--- resources/views/livewire/recently-reviewed-games.blade.php
+++ resources/views/livewire/recently-reviewed-games.blade.php
@@ -23,12 +23,31 @@
                             @endif
                         @endforeach
                     </div>
-                    <p class="mt-6 text-gray-400 hidden lg:block">{{$game['summary']}}
-                    </p>
+                    <p class="mt-6 text-gray-400 hidden lg:block">{{$game['summary']}}</p>
                 </div>
             </div>
         </div>
     @empty
-        <div class="spinner mt-8"></div>
+        @foreach(range(1,3) as $game)
+            <div class="recently-reviewed-container space-y-12 mt-8">
+                <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                    <div class="bg-gray-800 w-32 lg:w-48 h-40 lg:h-56"></div>
+                    <div class="ml-6 lg:ml-12">
+                        <div class="inline-block text-lg leading-tight text-transparent bg-gray-700 rounded mt-4">game title</div>
+                        <div class="mt-8 space-y-4 hidden lg:block">
+                            <span class="text-transparent bg-gray-700 rounded inline-block">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            </span>
+                            <span class="text-transparent bg-gray-700 rounded inline-block">
+                                Aliquid ea fugit in labore necessitatibus quasi,
+                            </span>
+                            <span class="text-transparent bg-gray-700 rounded inline-block">
+                                saepe sunt veniam! A animi consequuntur culpa cumque earum impedi
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     @endforelse
 </div>

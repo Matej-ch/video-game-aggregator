acebd34b1eacd2a54410a3e8357b780c9b562c15
Matej <matejchalachan@gmail.com>
2020-07-26 13:24:52 +0200

    modal not finnised , vuew added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,4 +28,14 @@
         </div>
     </div>
 
+
+    <x-modal
+    title="This modal title">
+
+        <x-slot name="trigger"><button @click="on = true">Show Modal</button></x-slot>
+        <h2>This is modal body</h2>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam at culpa cupiditate earum eius enim esse explicabo incidunt minima nam nobis quidem recusandae saepe sunt tenetur totam, unde veniam.
+
+    </x-modal>
+
 @endsection

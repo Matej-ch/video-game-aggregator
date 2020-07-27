2f918639841810c74fb508fd2e0694c62dec454b
Matej <matejchalachan@gmail.com>
2020-07-27 22:51:12 +0200

    finish dropdown with alpine

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,10 +1,29 @@
-<div>
 
+@props(['alignment' => 'left'])
+
+@php
+
+    $alignmentClasses = [
+        'left' => 'left-0',
+        'right' => 'right-0',
+    ]
+
+@endphp
+
+<div class="relative" x-data="{open: false}" @click.away="open = false">
     <div @click="open = !open">
         {{ $trigger }}
     </div>
 
-    <div class="absolute z-20 bg-white rounded shadow-md py-2 mt-1" v-show="open">
+    <div
+        class="absolute {{$alignmentClasses[$alignment]}} z-20 bg-white rounded shadow-md py-2 mt-1"
+        x-show="open"
+        x-transition:enter="transition duration-300 transform"
+        x-transition:enter-start="opacity-0 scale-95"
+        x-transition:enter-end="opacity-100 scale-100"
+        x-transition:leave="transition duration-300  transform"
+        x-transition:leave-start="opacity-100 scale-100"
+        x-transition:leave-end="opacity-0 scale-95">
         {{ $slot }}
     </div>
 </div>

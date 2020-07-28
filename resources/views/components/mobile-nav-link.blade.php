eb37df06fe69de434d60c912071320f414c78978
Matej <matejchalachan@gmail.com>
2020-07-28 21:43:04 +0200

    mobile naw link component

--- resources/views/components/mobile-nav-link.blade.php
+++ resources/views/components/mobile-nav-link.blade.php
@@ -1,9 +1,9 @@
 @props(['route'])
 
 @php
-$classes = /*Request::is($route) ? 'px-6 hover:text-blue-400 text-blue-500' :*/ 'px-6 hover:text-blue-400';
+    $classes = /*Request::is($route) ? 'px-6 hover:text-blue-400 text-blue-500' :*/ 'block py-2 rounded';
 @endphp
 
 <a {{ $attributes->merge(['class'=> $classes ]) }} href="{{--{{ route($route) }}--}}">
-{{$slot}}
+    {{$slot}}
 </a>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,15 +1,45 @@
-<header class="border-b border-gray-800 px-6 py-4 flex justify-between items-center">
+<header class="border-b border-gray-800 px-6 py-4" x-data="{open: false}">
 
-    <h1 class="font-bold tracking-widest uppercase">
-        <img src="" alt="LOGO HERE" class="w-32 flex-none">
-    </h1>
+    <div class="flex justify-between items-center">
+        <h1 class="font-bold tracking-widest uppercase">
+            <img src="" alt="LOGO HERE" class="w-32 flex-none">
+        </h1>
 
-    <nav class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4 py-6">
+        <nav class="hidden md:flex">
+            <x-nav-link route="games">Games</x-nav-link>
+            <x-nav-link route="reviews">Reviews</x-nav-link>
+            <x-nav-link route="soon">Coming soon</x-nav-link>
+        </nav>
 
-        <x-nav-link route="games">Games</x-nav-link>
-        <x-nav-link route="reviews">Reviews</x-nav-link>
-        <x-nav-link route="soon">Comming soon</x-nav-link>
-    </nav>
+        <div class="md:hidden">
+            <button @click="open = ! open">
+                <svg viewBox="0 0 20 20" width="20" x-show="! open">
+                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g id="icon-shape" fill="white">
+                            <path d="M0,3 L20,3 L20,5 L0,5 L0,3 Z M0,9 L20,9 L20,11 L0,11 L0,9 Z M0,15 L20,15 L20,17 L0,17 L0,15 Z" id="Combined-Shape"></path>
+                        </g>
+                    </g>
+                </svg>
+
+                <svg viewBox="0 0 20 20" width="20" x-show="open">
+                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g id="icon-shape" fill="white">
+                            <polygon id="Combined-Shape" points="10 8.58578644 2.92893219 1.51471863 1.51471863 2.92893219 8.58578644 10 1.51471863 17.0710678 2.92893219 18.4852814 10 11.4142136 17.0710678 18.4852814 18.4852814 17.0710678 11.4142136 10 18.4852814 2.92893219 17.0710678 1.51471863 10 8.58578644"></polygon>
+                        </g>
+                    </g>
+                </svg>
+            </button>
+        </div>
+
+    </div>
+
+    <!-- mobile navigation -->
+    <div x-show="open" class="mt-3">
+        <x-mobile-nav-link>Games</x-mobile-nav-link>
+        <x-mobile-nav-link>Reviews</x-mobile-nav-link>
+        <x-mobile-nav-link>Coming soon</x-mobile-nav-link>
+    </div>
+
 
     {{--<nav class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4 py-6">
 
